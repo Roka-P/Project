fd368afa0d259eff3ea78ec5ea12ed20a554b59b
ROKA <bartender0n1y@gmail.com>
2021-08-08 00:57:30 +0900

    update interface

--- AthletePrj/src/main/java/com/kosa/myapp/Athlete/dao/IAthleteRepository.java
+++ AthletePrj/src/main/java/com/kosa/myapp/Athlete/dao/IAthleteRepository.java
@@ -1,5 +1,22 @@
 package com.kosa.myapp.Athlete.dao;
 
-public interface IAthleteRepository {
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.kosa.myapp.Athlete.model.AthleteVO;
+
+public interface IAthleteRepository {
+	
+	List<AthleteVO> getAthList();
+	AthleteVO getAthInfo(int athid);
+	void updateAth(AthleteVO athlete);
+	void insertAth(AthleteVO athlete);
+	void deleteAth(@Param("athid") int athid, @Param("birthDate") String birthdate);
+	
+	List<Map<String, Object>> getAllSportsId();
+	
+	String getPassword(int adminid);
+	
 }
