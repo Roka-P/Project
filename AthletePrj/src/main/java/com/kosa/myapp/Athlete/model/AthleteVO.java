483934b08d3048213e0b9f7aaaa27f38e27523ab
moominie <86271759+moominie@users.noreply.github.com>
2021-08-09 10:57:18 +0900

    Add files via upload
    AthleteVO commit by moominie

--- AthletePrj/src/main/java/com/kosa/myapp/Athlete/model/AthleteVO.java
+++ AthletePrj/src/main/java/com/kosa/myapp/Athlete/model/AthleteVO.java
@@ -1,5 +1,124 @@
 package com.kosa.myapp.Athlete.model;
 
-public class AthleteVO {
+import java.sql.Date;
 
+public class AthleteVO {
+	private int id;
+	private String name;
+	private java.sql.Date birthDate;
+	private String gender;
+	private String sports;
+	private int goldSolo;
+	private int silverSolo;
+	private int bronzeSolo;
+	private int goldTeam;
+	private int silverTeam;
+	private int bronzeTeam;
+	private String prize;
+	
+	
+	public AthleteVO() {
+		super();
+	}
+	public AthleteVO(int id, String name, Date birthDate, String gender, String sports, 
+				int goldSolo, int silverSolo, int bronzeSolo, int goldTeam, int silverTeam, 
+				int bronzeTeam, String prize) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.sports = sports;
+		this.goldSolo = goldSolo;
+		this.silverSolo = silverSolo;
+		this.bronzeSolo = bronzeSolo;
+		this.goldTeam = goldTeam;
+		this.silverTeam = silverTeam;
+		this.bronzeTeam = bronzeTeam;
+		this.prize = prize;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public java.sql.Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(java.sql.Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getSports() {
+		return sports;
+	}
+	public void setSports(String sports) {
+		this.sports = sports;
+	}
+	public int getGoldSolo() {
+		return goldSolo;
+	}
+	public void setGoldSolo(int goldSolo) {
+		this.goldSolo = goldSolo;
+	}
+	public int getSilverSolo() {
+		return silverSolo;
+	}
+	public void setSilverSolo(int silverSolo) {
+		this.silverSolo = silverSolo;
+	}
+	public int getBronzeSolo() {
+		return bronzeSolo;
+	}
+	public void setBronzeSolo(int bronzeSolo) {
+		this.bronzeSolo = bronzeSolo;
+	}
+	public int getGoldTeam() {
+		return goldTeam;
+	}
+	public void setGoldTeam(int goldTeam) {
+		this.goldTeam = goldTeam;
+	}
+	public int getSilverTeam() {
+		return silverTeam;
+	}
+	public void setSilverTeam(int silverTeam) {
+		this.silverTeam = silverTeam;
+	}
+	public int getBronzeTeam() {
+		return bronzeTeam;
+	}
+	public void setBronzeTeam(int bronzeTeam) {
+		this.bronzeTeam = bronzeTeam;
+	}
+	public String getPrize() {
+		return prize;
+	}
+	public void setPrize(String prize) {
+		this.prize = prize;
+	}
+	@Override
+	public String toString() {
+		return "AthleteVO [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", gender=" + gender
+				+ ", sports=" + sports + ", goldSolo=" + goldSolo + ", silverSolo=" + silverSolo + ", bronzeSolo="
+				+ bronzeSolo + ", goldTeam=" + goldTeam + ", silverTeam=" + silverTeam + ", bronzeTeam=" + bronzeTeam
+				+ ", prize=" + prize + "]";
+	}
+	
+	
+	
 }
