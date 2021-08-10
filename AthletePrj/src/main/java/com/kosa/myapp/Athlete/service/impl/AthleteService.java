ccacaca80def8d35f2c74fa3171f2fdef5f59114
moominie <86271759+moominie@users.noreply.github.com>
2021-08-10 11:51:03 +0900

    조회기능 AthleteService commit
    commit!!!!!

--- AthletePrj/src/main/java/com/kosa/myapp/Athlete/service/impl/AthleteService.java
+++ AthletePrj/src/main/java/com/kosa/myapp/Athlete/service/impl/AthleteService.java
@@ -1,7 +1,28 @@
-package com.kosa.myapp.Athlete.service.impl;
+package com.kosa.myapp.athlete.service.impl;
 
-import com.kosa.myapp.Athlete.service.IAthleteService;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kosa.myapp.athlete.dao.IAthleteRepository;
+import com.kosa.myapp.athlete.model.AthleteVO;
+import com.kosa.myapp.athlete.service.IAthleteService;
+
+@Service
 public class AthleteService implements IAthleteService {
+
+	@Autowired
+	IAthleteRepository athleteRepository;
+	
+	@Override
+	public List<AthleteVO> getAthList() {
+		return athleteRepository.getAthList();
+	}
+
+	@Override
+	public AthleteVO getAthInfo(int athid) {
+		return athleteRepository.getAthInfo(athid);
+	}
 
 }
