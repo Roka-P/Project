705adb332c950cade42f3e0e8dc099f2faa59c28
moominie <86271759+moominie@users.noreply.github.com>
2021-08-10 11:51:59 +0900

    조회기능 iAthleteService 커밋!!
    yo!

--- AthletePrj/src/main/java/com/kosa/myapp/Athlete/service/IAthleteService.java
+++ AthletePrj/src/main/java/com/kosa/myapp/Athlete/service/IAthleteService.java
@@ -1,5 +1,13 @@
-package com.kosa.myapp.Athlete.service;
+package com.kosa.myapp.athlete.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.kosa.myapp.athlete.model.AthleteVO;
 
 public interface IAthleteService {
+
+	List<AthleteVO> getAthList(); //모든 선수의 정보 조회
+	AthleteVO getAthInfo(int athid); //지정한 선수의 모든 정보 조회
 
 }
